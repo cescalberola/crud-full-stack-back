de5ec24e851285376af1e31b26a67ffcf1e1eabe
francesc.alberola <francesc.alberola@nter.es>
2025-04-14 16:40:54 +0200

    validation-mail

--- src/main/java/com/cescdev/crud_fullstack_angular/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cescdev/crud_fullstack_angular/exception/GlobalExceptionHandler.java
@@ -2,27 +2,49 @@ package com.cescdev.crud_fullstack_angular.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
- class GlobalExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorDetails>handleResourceNotFoundException(
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(
             ResourceNotFoundException ex,
             WebRequest webRequest
-    ){
-        ErrorDetails error = new ErrorDetails (
+    ) {
+        ErrorDetails error = new ErrorDetails(
                 LocalDateTime.now(),
-        ex.getMessage(),
-        webRequest.getDescription(false),
-        "NOT FOUND"
-                );
+                ex.getMessage(),
+                webRequest.getDescription(false),
+                "NOT_FOUND"
+        );
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDetails> handleValidationException(
+            MethodArgumentNotValidException ex,
+            WebRequest request
+    ) {
+        String message = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        ErrorDetails error = new ErrorDetails(
+                LocalDateTime.now(),
+                message,
+                request.getDescription(false),
+                "VALIDATION_FAILED"
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
